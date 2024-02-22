2ba0781cb59493ba0ce53d1a50c81b8bf1fbc3a7
Bryan Kwan <bryan.kwan@ymail.com>
2024-02-21 21:58:13 -0800

    Added more filters
    Specifically added location filters to filter store by miles away from
    UCLA

--- filter.py
+++ filter.py
@@ -18,22 +18,22 @@ def findByName(data, shopName):
     )
 
 
-def findRatingsGT(data, num):
+def find_ratings_GT(data, num):
     return data.find({
         "averageRating": {"$gt" : num}
     }, noId)
 
-def findNumRatingsGT(data, num):
+def find_num_ratings_GT(data, num):
     return data.find({
         "numRatings": {"$gt": num}
     }, noId)
 
-def findCostLT(data, num): 
+def find_cost_LT(data, num): 
     return data.find({
 
     })
 
-def generalFind(data, field, value, compare=None):
+def general_find(data, field, value, compare=None):
     if compare:
         result = data.find({
         field: {compare: value}
@@ -65,43 +65,72 @@ def calculate_distance(lat1, lon1, lat2, lon2):
     distance_miles = distance * 0.621371
     
     return distance_miles
-'''with open('data.csv', 'r') as csvfile:
-    csvreader = csv.DictReader(csvfile)
-    for row in csvreader:
-        latitude = float(row['lat'])
-        longitude = float(row['long'])
-        
-        collection.insert_one({
-            'name': row['name'],
-            'location': {
-                'address': row['address'],
-                'coordinates': [latitude, longitude] 
-            },
-            'averageRating': float(row['averageRating']),
-            'numRatings': int(row['numRatings']),
-            'cost': int(row['cost']),
-            'features': row['features'],
-            'ratings': row['ratings']
-        })'''
 
-lat1, lon1 = 40.7128, -74.0060
+def find_location(data, num):
+    lat1 = 34.068920
+    lon1 = -118.445183
+    less = []
+    result = data.find()
+    for shop in result:
+        lat2 = shop['location']['coordinates'][0]
+        lon2 = shop['location']['coordinates'][1]
+        distance_from = calculate_distance(lat1, lon1, lat2, lon2)
+        if (distance_from <= num):
+            less.append(shop)
+    return less
+
+
+if __name__ == "__main__":
+    # Coordinates for New York City
+    lat1, lon1 = 40.7128, -74.0060
     
-# Coordinates for Los Angeles
-lat2, lon2 = 34.0522, -118.2437
+    # Coordinates for Los Angeles
+    lat2, lon2 = 34.0522, -118.2437
+    
+    # Calculate the distance
+    distance = calculate_distance(lat1, lon1, lat2, lon2)
+    
+    print("The distance between New York City and Los Angeles is {:.2f} miles.".format(distance))        
+    '''with open('data.csv', 'r') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        for row in csvreader:
+            latitude = float(row['lat'])
+            longitude = float(row['long'])
+            
+            collection.insert_one({
+                'name': row['name'],
+                'location': {
+                    'address': row['address'],
+                    'coordinates': [latitude, longitude] 
+                },
+                'averageRating': float(row['averageRating']),
+                'numRatings': int(row['numRatings']),
+                'cost': int(row['cost']),
+                'features': row['features'],
+                'ratings': row['ratings']
+            })'''
 
-# Calculate the distance
-distance = calculate_distance(lat1, lon1, lat2, lon2)
+    for results in find_location(collection, 5):
+        print(results)
+    '''
+    lat1, lon1 = 40.7128, -74.0060
+        
+    # Coordinates for Los Angeles
+    lat2, lon2 = 34.0522, -118.2437
 
-print("The distance between New York City and Los Angeles is {:.2f} miles.".format(distance))
-'''for results in findByName(collection, "Ministry of Coffee"):
-    print(results)
-print('\n')
-for results in findRatingsGT(collection, 3.6):
-    print(results)
-print('\n')
-for results in findNumRatingsGT(collection, 700):
-    print(results)
+    # Calculate the distance
+    distance = calculate_distance(lat1, lon1, lat2, lon2)
 
-print('\n')
-for results in generalFind(collection, 'numRatings', 700, '$gt'):
-    print(results)'''
+    print("The distance between New York City and Los Angeles is {:.2f} miles.".format(distance))
+    for results in findByName(collection, "Ministry of Coffee"):
+        print(results)
+    print('\n')
+    for results in findRatingsGT(collection, 3.6):
+        print(results)
+    print('\n')
+    for results in findNumRatingsGT(collection, 700):
+        print(results)
+
+    print('\n')
+    for results in generalFind(collection, 'numRatings', 700, '$gt'):
+        print(results)'''
--- insert.py
+++ insert.py
@@ -1,9 +1,13 @@
 import csv
 from pymongo import MongoClient
 
+wantToReset = False
 client = MongoClient('mongodb://localhost:27017/')
 db = client['database'] 
-collection = db['coffee_shops']  
+collection = db['coffee_shops']
+if wantToReset:
+    collection.delete_many({})
+    print("Collection cleared to insert more")
 
 with open('data.csv', 'r') as csvfile:
     csvreader = csv.DictReader(csvfile)
