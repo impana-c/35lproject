7a8405cd4872c873b653c6dfcb4180cf81d14e1b
AnishThalamati <61165687+AnishThalamati@users.noreply.github.com>
2024-03-11 19:01:02 -0700

    minor bug fixes and integration with log/sign in page

--- App/client/src/App.jsx
+++ App/client/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-//import Signup from './Signin/Signup'
-//import Login from './Signin/Login' 
+import Signup from './Signin/Signup'
+import Login from './Signin/Login' 
 import Homepage from './Homepage/Homepage'
 import Profile from './Profile/Profile'
 import UpdateProfile from './Profile/UpdateProfile'
@@ -22,6 +22,9 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Signup/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
         <Route path='/home' element={<Homepage/>}/>
         <Route path='/profile' element={<Profile/>}/>
         <Route path='/updateprofile' element={<UpdateProfile/>}/>
