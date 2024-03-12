e6891a1533f0c7af1c0007cd80998f166764baeb
harsha <harshakosuri21@gmail.com>
2024-03-12 16:11:25 -0700

    Styled UpdateProfile

--- App/client/src/Profile/Profile.jsx
+++ App/client/src/Profile/Profile.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './Profile.css'
+import './Login.css'; //imports the css file 
+
 
 function Profile() {
   const [user, setUser] = useState(null);
